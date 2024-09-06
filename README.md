6c2cb5a5307a089d7d546535149ebe4f2fbec71d
Hans Strausl <strauslh@gmail.com>
2024-09-05 17:19:03 -0700

    initial working state

---
+++ README.md
@@ -0,0 +1,3 @@
+# Seashells client written in Rust
+
+Wrote a port of the seashells client in Go years ago, now writing one in Rust as a learning exercise.
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::net;
+use std::io::{self, prelude::*};
+
+fn main() -> io::Result<()> {
+    let target = "seashells.io:1337";
+    let mut stream = net::TcpStream::connect(target)?;
+    let mut string = String::new();
+    let mut buffer: Vec<u8> = vec![0; 64];
+
+    stream.read(&mut buffer)?;
+    let string = String::from_utf8(buffer)
+        .expect("read into buffer");
+    // stream.read_to_string(&mut string)?;
+    println!("{}", string);
+    let mut stdin = io::stdin();
+
+    let mut quit = false;
+    while !quit {
+        let mut input_buffer: Vec<u8> = vec![0; 128];
+        stdin.read(&mut input_buffer)?;
+        // let input_string = String::from_utf8(input_buffer)
+            // .expect("input buffer");
+        // stream.read_to_string(&mut string)?;
+        // println!("{}", input_string);
+        stream.write(&mut input_buffer)?;
+
+    }
+
+
+    Ok(())
+}
