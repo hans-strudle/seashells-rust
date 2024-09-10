c1b350eea474caabd216a3f6c675ed1dbee639c4
Hans Strausl <strauslh@gmail.com>
2024-09-09 20:57:44 -0700

    add clap parser; better stdin read

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
 use std::net;
 use std::io::{self, prelude::*};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = "seashells.io client written in Rust")]
+struct Cli {
+
+    #[arg(short, long, default_value = "seashells.io")]
+    url: String,
+    
+    #[arg(short, long, default_value = "1337")]
+    port: String,
+
+    #[arg(short, long, default_value = "false")]
+    quiet: bool,
+}
 
 fn main() -> io::Result<()> {
-    let target = "seashells.io:1337";
+    let cli = Cli::parse();
+
+    let target = format!("{}:{}", cli.url, cli.port);
+
     let mut stream = net::TcpStream::connect(target)?;
-    let string = String::new();
     let mut buffer: Vec<u8> = vec![0; 64];
 
     stream.read(&mut buffer)?;
     let string = String::from_utf8(buffer)
         .expect("read into buffer");
-    // stream.read_to_string(&mut string)?;
-    print!("{}", string);
-    let mut stdin = io::stdin();
+    print!("{string}");
 
-    let quit = false;
-    while !quit {
-        let mut input_buffer: Vec<u8> = vec![0; 256];
-        stdin.read(&mut input_buffer)?;
-        let input_string = String::from_utf8(input_buffer.clone())
-            .expect("input buffer");
-        // stream.read_to_string(&mut string)?;
-        print!("{}", input_string);
-        stream.write(&mut input_buffer)?;
+    let stdin = io::stdin();
+    loop {
+        let mut input_buffer = String::new();
+        stdin.read_line(&mut input_buffer)?;
+        if !cli.quiet {
+            print!("{input_buffer}");
+        }
+        stream.write(&mut input_buffer.as_bytes())?;
     }
-
     Ok(())
 }
